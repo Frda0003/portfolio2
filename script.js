4c0976f46818b6aa76aabd983ea14f2404d7c18d
Frederik Holm Damgaard <frda0003@stud.kea.dk>
2025-01-06 15:29:05 +0100

    gitignore test

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+////// Header funktionalitet https://frederikdamgaard14.dk/kea/02_web/01_website/website/index.html ////////////
+
 document.getElementById("menu-icon").addEventListener("click", function () {
   const navLinks = document.getElementById("nav-links");
   navLinks.classList.toggle("show"); // Toggler 'show' klassen
 });
 
-// Adding event listeners to each button
+// ChatGPT
+/////////////// Adding event listeners to each button //////////////////
 const buttons = document.querySelectorAll('button[id^="project_button"]');
 
 buttons.forEach((button) => {
@@ -15,3 +18,15 @@ buttons.forEach((button) => {
     window.open(url, "_blank");
   });
 });
+// ChatGPT
+
+/////////////////// Videooverlay funktion ////////////
+
+const video = document.querySelector("#video");
+const overlayVideo = document.querySelector("#overlay_video");
+
+video.addEventListener("ended", showOverlay);
+
+function showOverlay() {
+  overlayVideo.style.display = "flex";
+}
